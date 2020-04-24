5f218c98bae4b03ddf809315b08992ffe31806d3
alistair <alistairholmes95@gmail.com>
2020-04-24 11:13:02 +0200

    init commit

---
+++ android/app/src/main/kotlin/co/zw/alistair/loginkit/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.zw.alistair.loginkit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/login/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff251F34),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
